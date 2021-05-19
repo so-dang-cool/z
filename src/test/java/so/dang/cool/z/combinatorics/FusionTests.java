c5b647ee42fc1ac95c1421dcb88f9aefe4290fed
♪ hiljusti 🎮 <hiljusti@pm.me>
2021-05-19 16:46:51 -0700

    Add Supplier fusions and tests

--- src/test/java/so/dang/cool/z/combinatorics/FusionTests.java
+++ src/test/java/so/dang/cool/z/combinatorics/FusionTests.java
@@ -1876,7 +1876,142 @@ public class FusionTests {
         assertTrue(Z.fuse(isLongThree, maybeNot).apply(3L).test(false));
     }
 
-    // TODO: Supplier
+    @Test
+    void sup_to_fn() {
+        assertEquals(suppliedString.toLowerCase(), Z.fuse(getString, toLower).get());
+    }
+
+    @Test
+    void sup_to_bifn() {
+        assertEquals("Z!", Z.fuse(getString, concat).apply("!"));
+    }
+
+    @Test
+    void sup_to_toDblFn() {
+        assertThrows(NumberFormatException.class, () -> Z.fuse(getString, stringToDouble).getAsDouble());
+    }
+
+    @Test
+    void sup_to_toDblBifn() {
+        assertThrows(NumberFormatException.class, () -> Z.fuse(getString, addStringsAsDouble).applyAsDouble("1.0"));
+    }
+
+    @Test
+    void sup_to_toIntFn() {
+        assertThrows(NumberFormatException.class, () -> Z.fuse(getString, stringToInt).getAsInt());
+    }
+
+    @Test
+    void sup_to_toIntBifn() {
+        assertThrows(NumberFormatException.class, () -> Z.fuse(getString, addStringsAsInt).applyAsInt("1"));
+    }
+
+    @Test
+    void sup_to_toLongFn() {
+        assertThrows(NumberFormatException.class, () -> Z.fuse(getString, stringToLong).getAsLong());
+    }
+
+    @Test
+    void sup_to_toLongBifn() {
+        assertThrows(NumberFormatException.class, () -> Z.fuse(getString, addStringsAsLong).applyAsLong("1"));
+    }
+
+    @Test
+    void sup_to_pred() {
+        assertFalse(Z.fuse(getString, isEmpty).getAsBoolean());
+    }
+
+    @Test
+    void sup_to_bipred() {
+        assertTrue(Z.fuse(getString, startsWith).test("Z"));
+    }
+
+    @Evil
+    @Test
+    void sup_to_cns() {
+        synchronized(consumedStringA) {
+            consumedStringA = "";
+
+            Z.fuse(getString, saveStringA).run();
+
+            assertEquals(suppliedString, consumedStringA);
+        }
+    }
+
+    @Evil
+    @Test
+    void sup_to_bicns() {
+        synchronized(consumedStringB) {
+            synchronized(consumedStringC) {
+                consumedStringB = "";
+                consumedStringC = "";
+
+                Z.fuse(getString, saveStringsBandC).accept("z");
+
+                assertEquals(suppliedString, consumedStringB);
+                assertEquals("z", consumedStringC);
+            }
+        }
+    }
+
+    @Evil
+    @Test
+    void sup_to_objDblCns() {
+        synchronized(consumedStringD) {
+            synchronized(consumedDoubleB) {
+                consumedStringD = "";
+                consumedDoubleB = 0.0;
+
+                Z.fuse(getString, saveStringDDoubleB).accept(5.0);
+
+                assertEquals(suppliedString, consumedStringD);
+                assertEquals(5.0, consumedDoubleB);
+            }
+        }
+    }
+
+    @Evil
+    @Test
+    void sup_to_objIntCns() {
+        synchronized(consumedStringE) {
+            synchronized(consumedIntB) {
+                consumedStringE = "";
+                consumedIntB = 0;
+
+                Z.fuse(getString, saveStringEIntB).accept(6);
+
+                assertEquals(suppliedString, consumedStringE);
+                assertEquals(6, consumedIntB);
+            }
+        }
+    }
+
+    @Evil
+    @Test
+    void sup_to_objLongFn() {
+        synchronized(consumedStringF) {
+            synchronized(consumedLongB) {
+                consumedStringF = "";
+                consumedLongB = 0L;
+
+                Z.fuse(getString, saveStringFLongB).accept(7L);
+
+                assertEquals(suppliedString, consumedStringF);
+                assertEquals(7L, consumedLongB);
+            }
+        }
+    }
+
+    @Test
+    void sup_to_toUnop() {
+        assertEquals("Z?", Z.fuse(getString, addQuestionMark).get());
+    }
+
+    @Test
+    void sup_to_toBiop() {
+        assertEquals("same-ish", Z.fuse(getString, relation).apply("z"));
+    }
+
     // TODO: BooleanSupplier
     // TODO: DoubleSupplier
     // TODO: IntSupplier
