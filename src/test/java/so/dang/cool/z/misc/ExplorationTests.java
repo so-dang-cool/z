3a27e029141c1a1171c660f0a9b48d613a2946ef
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2023-04-04 09:03:21 -0700

    Remove supplier thing

--- src/test/java/so/dang/cool/z/misc/ExplorationTests.java
+++ src/test/java/so/dang/cool/z/misc/ExplorationTests.java
@@ -2,56 +2,11 @@ package so.dang.cool.z.misc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.time.Duration;
-import java.time.Instant;
-import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import org.junit.jupiter.api.Test;
 
 public class ExplorationTests {
-
-    @Test
-    public void decoratedSuppliers() {
-        Supplier<Integer> threeSup = () -> {
-            try {
-                Thread.sleep(Duration.ofSeconds(3).toMillis());
-            } catch (InterruptedException _e) {
-                // Whatever man.
-            }
-            return 3;
-        };
-
-        Supplier<Integer> timedThreeSup = Suppliers.timed(
-            threeSup,
-            dur ->
-                System.out.printf("Getting three took: %sms\n", dur.toMillis())
-        );
-
-        System.out.println("Getting 3...");
-        int three = timedThreeSup.get();
-        System.out.println("Done");
-
-        assertEquals(3, three);
-    }
-
-    static class Suppliers {
-
-        public static <T> Supplier<T> timed(
-            Supplier<T> sup,
-            Consumer<Duration> elapsed
-        ) {
-            return () -> {
-                Instant before = Instant.now();
-                T result = sup.get();
-                Instant after = Instant.now();
-
-                elapsed.accept(Duration.between(before, after));
-
-                return result;
-            };
-        }
-    }
 
     @Test
     public void uhawwwwwww_example() {
